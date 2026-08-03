ad8fafa93994f1285265547513ad9d7e238731d9
Shayne-Gao <shayne.gao@outlook.com>
2026-08-03 14:52:37 +0800

    release(apk): bump to v0.1.95 and simplify tracking button

--- apk/app/build.gradle.kts
+++ apk/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.shayne.fogvisitor"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.1.94-alpha"
+        versionCode = 26
+        versionName = "0.1.95-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
