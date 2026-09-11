1d5dbf47abb41452b03f051de8acc65b383e283c
Shayne-Gao <shayne.gao@outlook.com>
2026-09-11 23:22:07 +0800

    Improve fog layer rendering performance

--- apk/app/build.gradle.kts
+++ apk/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.shayne.fogvisitor"
         minSdk = 26
         targetSdk = 35
-        versionCode = 37
-        versionName = "0.1.106-alpha"
+        versionCode = 38
+        versionName = "0.1.107-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
